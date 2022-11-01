7549a5521092dea23d9c85faafd1c6e5464e8730
Kathlen <kathlengraziela@gmail.com>
2022-11-01 14:07:22 -0300

    Add project files.

---
+++ ExercicioUdemy3/Produto.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Globalization;
+
+namespace ExercicioUdemy3
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco, ValorTotal;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
